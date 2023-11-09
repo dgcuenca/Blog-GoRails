0f6c25fd5946e9799de457e22c949aa30d4f18b3
diego <dgcuenca@gmail.com>
2023-11-09 16:32:26 -0500

    Enable user create a new blog post

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  #routes are reading in order since we have a rescue blog_post we have to write first the new action
-  #otherwise show will take as id new and produce an error that redirect to root route.
+  #routes are reading in order since we have a rescue in blog_post we have to write first the new action
+  #otherwise show will take as id "new" and produce an error that redirect to root route.
   get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post # enable helpers as blog_post_path blog_post_url
+  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post # enable helpers as blog_post_path blog_post_url also helps write less code, rails conventions
   # Defines the root path route ("/")
   root "blog_posts#index"
 end
