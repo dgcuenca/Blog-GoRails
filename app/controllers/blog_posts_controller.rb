863fc700e326b4a3be4e7be776540d4cedb7e143
diego <dgcuenca@gmail.com>
2023-11-09 13:11:55 -0500

    Show post in root view

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,4 +1,6 @@
 class BlogPostsController < ApplicationController
   def index
+    #instance variable are used because rails can use these in .erb template
+    @blog_posts = BlogPost.all
   end
 end
