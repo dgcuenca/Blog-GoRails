b73ffe6a3d266a91cbdbf24686b7a052348f8e92
diego <dgcuenca@gmail.com>
2023-11-09 14:04:00 -0500

    Add links to posts to read individually

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post # enable helpers as blog_post_path blog_post_url
   # Defines the root path route ("/")
   root "blog_posts#index"
 end
