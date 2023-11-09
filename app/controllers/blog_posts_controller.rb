1347faa72cab91d221efd6116db1b8c6300f1f35
diego <dgcuenca@gmail.com>
2023-11-09 13:41:39 -0500

    Display individual posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -3,4 +3,8 @@ class BlogPostsController < ApplicationController
     #instance variable are used because rails can use these in .erb template
     @blog_posts = BlogPost.all
   end
+
+  def show
+    @blog_post = BlogPost.find(params[:id])
+  end
 end
