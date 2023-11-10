6a604c537f7613e211133a62431895ed13935738
diego <dgcuenca@gmail.com>
2023-11-10 18:47:36 -0500

    Manage a user request to update a blog

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   #otherwise show will take as id "new" and produce an error that redirect to root route.
   get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post # enable helpers as blog_post_path blog_post_url also helps write less code, rails conventions
+  patch "/blog_posts/:id", to: "blog_posts#update" #we dont need a name, rails is going to take blog_post helper and generate the same url 
   get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
   post "/blog_posts", to: "blog_posts#create", as: :blog_posts #rails looks for a plural version when try to find a @blog_post without id
 
