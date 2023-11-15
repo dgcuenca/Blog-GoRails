c09314ab09364f04ba3be289dfb1321553ea8a46
diego <dgcuenca@gmail.com>
2023-11-15 12:30:23 -0500

    Manage query different type of blogpost

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,11 @@
 class BlogPost < ApplicationRecord
   validates :title, presence: true
   validates :body, presence:true
+
+  # scope enable to use BlogPost.draft 
+  scope :draft, -> { where(published_at: nil) }
+  # Use staby lambda in order to evaluate everytime we make a querry for example when we do BlogPost.scheduled
+  #If not Time.current is going to take the value of the first query and dont change after that
+  scope :published, -> { where("published_at <= ?", Time.current) }
+  scope :scheduled, -> { where("published_at > ?", Time.current) }
 end
