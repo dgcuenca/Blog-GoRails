953d1bda7930bdcd3d4a95bb941c819fb20d7024
diego <dgcuenca@gmail.com>
2023-11-20 16:35:36 -0500

    Get queries and paginate it

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -5,6 +5,7 @@ class BlogPostsController < ApplicationController
   def index
     #instance variable are used because rails can use these in .erb template
     @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
+    @pagy, @blog_posts = pagy(@blog_posts)
   end
 
   def show
