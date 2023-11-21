6b7521b0c5cf2e4fa9c80af19c749020645dd35f
diego <dgcuenca@gmail.com>
2023-11-21 15:39:11 -0500

    Preserv not saved data when delete cover imagen

--- app/controllers/blog_posts/cover_images_controller.rb
+++ app/controllers/blog_posts/cover_images_controller.rb
@@ -1,10 +1,18 @@
 class BlogPosts::CoverImagesController < ApplicationController
+  #in order to use helper dom_id()
+  include ActionView::RecordIdentifier
+
   before_action :authenticate_user!
   before_action :set_blog_post
 
   def destroy
     @blog_post.cover_image.purge_later
-    redirect_to edit_blog_post_path(@blog_post)
+    #not redirect in order to not lose data not saved in the form and only delete cover image
+    #keep this for any browser dont have JS enable
+    respond_to do |format|
+      format.html { redirect_to edit_blog_post_path(@blog_post) }
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(dom_id(@blog_post, :cover_image))}
+    end
   end
 
   private
